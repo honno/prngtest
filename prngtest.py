a4908991ca286d6ae44e995ee5e6cbae756176de
Matthew Barber <quitesimplymatt@gmail.com>
2021-09-07 20:49:31 +0100

    `apen()` randomness test

--- prngtest.py
+++ prngtest.py
@@ -1,7 +1,6 @@
 from bisect import bisect_left
 from collections import defaultdict
 from functools import lru_cache
-from itertools import product
 from math import erfc, log, sqrt
 from numbers import Real
 from typing import (Dict, Iterable, Iterator, List, Literal, NamedTuple, Tuple,
@@ -9,7 +8,7 @@ from typing import (Dict, Iterable, Iterator, List, Literal, NamedTuple, Tuple,
 
 import numpy as np
 from bitarray import bitarray, frozenbitarray
-from bitarray.util import ba2int, zeros
+from bitarray.util import ba2int, int2ba, zeros
 from scipy.fft import fft
 from scipy.special import gammaincc
 from scipy.stats import chisquare
@@ -61,6 +60,7 @@ class ResultsMap(dict):
 def monobit(bits) -> Result:
     a = frozenbitarray(bits)
     n = len(a)
+
     ones = a.count(1)
     zeros = n - ones
     diff = abs(ones - zeros)
@@ -151,14 +151,14 @@ def longest_runs(bits):
         ) from e
     nblocks, blocksize, intervals = defaults[key]
 
-    maxlen_bins = {k: 0 for k in intervals}
+    max_len_bins = {k: 0 for k in intervals}
     for chunk in _chunked(a, nblocks, blocksize):
         one_run_lengths = [len_ for val, len_ in _asruns(chunk) if val == 1]
         try:
-            maxlen = max(one_run_lengths)
+            max_len = max(one_run_lengths)
         except ValueError:
-            maxlen = 0
-        maxlen_bins[_binkey(intervals, maxlen)] += 1
+            max_len = 0
+        max_len_bins[_binkey(intervals, max_len)] += 1
 
     blocksize_probs = {
         # blocksize: <bin interval probs>
@@ -168,25 +168,25 @@ def longest_runs(bits):
         1000: [0.1307, 0.2437, 0.2452, 0.1714, 0.1002, 0.1088],
         10000: [0.0882, 0.2092, 0.2483, 0.1933, 0.1208, 0.0675, 0.0727],
     }
-    expected_bincounts = [prob * nblocks for prob in blocksize_probs[blocksize]]
+    expected_bin_counts = [prob * nblocks for prob in blocksize_probs[blocksize]]
 
-    chi2, p = chisquare(list(maxlen_bins.values()), expected_bincounts)
+    chi2, p = chisquare(list(max_len_bins.values()), expected_bin_counts)
 
     return Result(chi2, p)
 
 
 def _gf2_matrix_rank(matrix: Iterable[bitarray]) -> int:
-    numbers = [ba2int(a) for a in matrix]
+    nums = [ba2int(a) for a in matrix]
 
     rank = 0
-    while len(numbers) > 0:
-        pivot = numbers.pop()
+    while len(nums) > 0:
+        pivot = nums.pop()
         if pivot:
             rank += 1
             lsb = pivot & -pivot
-            for i, num in enumerate(numbers):
+            for i, num in enumerate(nums):
                 if lsb & num:
-                    numbers[i] = num ^ pivot
+                    nums[i] = num ^ pivot
 
     return rank
 
@@ -203,7 +203,6 @@ def matrix_rank(bits, matrix_dimen: Tuple[int, int]) -> Result:
         matrix = _chunked(chunk, nrows, ncols)
         rank = _gf2_matrix_rank(matrix)
         ranks.append(rank)
-
     fullrank = min(nrows, ncols)
     rankcounts = [0, 0, 0]
     for rank in ranks:
@@ -251,6 +250,11 @@ def _windowed(a: bitarray, blocksize: int) -> Iterator[bitarray]:
         yield a[i:i + blocksize]
 
 
+def _product(length: int) -> Iterator[frozenbitarray]:
+    for n in range(2 ** length):
+        yield frozenbitarray(int2ba(n, length=length))
+
+
 def notm(bits, tempsize: int, blocksize: int) -> Dict[bitarray, Result]:
     a = frozenbitarray(bits)
     n = len(a)
@@ -259,15 +263,15 @@ def notm(bits, tempsize: int, blocksize: int) -> Dict[bitarray, Result]:
     block_counts = defaultdict(lambda: defaultdict(int))
     for i, chunk in enumerate(_chunked(a, nblocks, blocksize)):
         matches = defaultdict(int)
-        for temp in _windowed(chunk, tempsize):
-            matches[temp] += 1
+        for window in _windowed(chunk, tempsize):
+            matches[window] += 1
         for temp, count in matches.items():
             block_counts[temp][i] = count
 
     count_expect = (blocksize - tempsize + 1) / 2 ** tempsize
     variance = blocksize * ((1 / 2 ** tempsize) - ((2 * tempsize - 1) / 2 ** (2 * tempsize)))
     results = ResultsMap()
-    for temp in (frozenbitarray(p) for p in product([0, 1], repeat=tempsize)):
+    for temp in _product(tempsize):
         count_diffs = [block_counts[temp][b] - count_expect for b in range(nblocks)]
         chi2 = sum(diff ** 2 / variance for diff in count_diffs)
         p = gammaincc(nblocks / 2, chi2 / 2)
@@ -281,16 +285,12 @@ def otm(bits, tempsize: int, blocksize: int) -> Result:
     n = len(a)
     nblocks = n // blocksize
     a = a[: nblocks * blocksize]
-    temp = frozenbitarray(1 for _ in range(tempsize))
+    temp = ~zeros(tempsize)
 
     block_matches = []
     for chunk in _chunked(a, nblocks, blocksize):
-        matches = 0
-        for window in _windowed(chunk, tempsize):
-            if window == temp:
-                matches += 1
+        matches = sum(window == temp for window in _windowed(chunk, tempsize))
         block_matches.append(matches)
-
     tallies = [0, 0, 0, 0, 0, 0]
     for matches in block_matches:
         tallies[min(matches, 5)] += 1
@@ -330,10 +330,10 @@ def universal(bits, blocksize: int, init_nblocks: int) -> Result:
     for i, temp in enumerate(_chunked(init, init_nblocks, blocksize), 1):
         last_temp_indices[temp] = i
 
-    log2_gaps = 0
+    log2_gaps_acc = 0
     for i, temp in enumerate(_chunked(test, test_nblocks, blocksize), init_nblocks + 1):
         gap = i - last_temp_indices[temp]
-        log2_gaps += log(gap, 2)
+        log2_gaps_acc += log(gap, 2)
         last_temp_indices[temp] = i
 
     blocksize_probs = {
@@ -357,7 +357,7 @@ def universal(bits, blocksize: int, init_nblocks: int) -> Result:
     }
     mean_expect, variance = blocksize_probs[blocksize]
 
-    norm_gaps = log2_gaps / test_nblocks
+    norm_gaps = log2_gaps_acc / test_nblocks
     p = erfc(abs((norm_gaps - mean_expect) / (sqrt(2 * variance))))
 
     return Result(norm_gaps, p)
@@ -409,9 +409,9 @@ def complexity(bits, blocksize: int) -> Result:
         variance_bins[_binkey(intervals, variance)] += 1
 
     probs = [0.010417, 0.03125, 0.125, 0.5, 0.25, 0.0625, 0.020833]
-    expected_bincounts = [nblocks * prob for prob in probs]
+    expected_bin_counts = [nblocks * prob for prob in probs]
 
-    chi2, p = chisquare(list(variance_bins.values()), expected_bincounts)
+    chi2, p = chisquare(list(variance_bins.values()), expected_bin_counts)
 
     return Result(chi2, p)
 
@@ -420,31 +420,47 @@ def serial(bits, blocksize) -> Tuple[Result, Result]:
     a = frozenbitarray(bits)
     n = len(a)
 
-    normsums = {}
-    for window_size in [blocksize, blocksize - 1, blocksize - 2]:
-        if window_size > 0:
-            ouroboros = a + a[: window_size - 1]
+    norm_sums = {}
+    for tempsize in [blocksize, blocksize - 1, blocksize - 2]:
+        if tempsize > 0:
+            ouroboros = a + a[: tempsize - 1]
             counts = defaultdict(int)
-            for window in _windowed(ouroboros, window_size):
+            for window in _windowed(ouroboros, tempsize):
                 counts[window] += 1
             sum_squares = sum(count ** 2 for count in counts.values())
-            normsum = (2 ** window_size / n) * sum_squares - n
+            norm_sums[tempsize] = (2 ** tempsize / n) * sum_squares - n
         else:
-            normsum = 0
-        normsums[window_size] = normsum
+            norm_sums[tempsize]
 
-    normsum_delta1 = normsums[blocksize] - normsums[blocksize - 1]
-    p1 = gammaincc(2 ** (blocksize - 2), normsum_delta1 / 2)
-    normsum_delta2 = normsums[blocksize] - 2 * normsums[blocksize - 1] + normsums[blocksize - 2]
+    norm_sum_delta1 = norm_sums[blocksize] - norm_sums[blocksize - 1]
+    p1 = gammaincc(2 ** (blocksize - 2), norm_sum_delta1 / 2)
+    normsum_delta2 = norm_sums[blocksize] - 2 * norm_sums[blocksize - 1] + norm_sums[blocksize - 2]
     p2 = gammaincc(2 ** (blocksize - 3), normsum_delta2 / 2)
 
     return ResultsTuple(
-        (Result(normsum_delta1, p1), Result(normsum_delta2, p2))
+        (Result(norm_sum_delta1, p1), Result(normsum_delta2, p2))
     )
 
 
-def apen(bits, **kwargs):
-    pass
+def apen(bits, blocksize: int) -> Result:
+    a = frozenbitarray(bits)
+    n = len(a)
+
+    phis = []
+    for tempsize in [blocksize, blocksize + 1]:
+        ouroboros = a + a[: tempsize - 1]
+        temp_counts = defaultdict(int)
+        for window in _windowed(ouroboros, tempsize):
+            temp_counts[window] += 1
+        norm_counts = [count / n for count in temp_counts.values()]
+        phi = sum(count * log(count) for count in norm_counts)
+        phis.append(phi)
+
+    apen = phis[0] - phis[1]
+    chi2 = 2 * n * (log(2) - apen)
+    p = gammaincc(2 ** (blocksize - 1), chi2 / 2)
+
+    return Result(chi2, p)
 
 
 def cusum(bits, **kwargs):
