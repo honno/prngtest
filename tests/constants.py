888734a0bd2d2656f898dceac23ea948013f7a4c
Matthew Barber <quitesimplymatt@gmail.com>
2022-02-27 13:28:02 +0000

    Remove some awkward smoke tests

--- tests/constants.py
+++ tests/constants.py
@@ -2,9 +2,16 @@ from pathlib import Path
 
 from bitarray import bitarray
 
-__all__ = ["e"]
+__all__ = ["e", "sha1"]
+
+
+data_path = Path(__file__).parent / "data"
 
 e = bitarray()
-with open(Path(__file__).parent / "data" / "e.bin", "rb") as f:
+with open(data_path / "e.bin", "rb") as f:
     e.fromfile(f)
-e = e[:1_000_000]
+e = e[:1_000_000]  # convenience as only first million bits is used in examples
+
+sha1 = bitarray()
+with open(data_path / "sha1.bin", "rb") as f:
+    sha1.fromfile(f)
--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -3,10 +3,6 @@ from bitarray.util import urandom
 
 from prngtest import *
 
-# TODO use SHA-1 data
-a = urandom(1_000_000)
-a[1] = 1 - a[0]  # ensures mixed bits when slicing
-
 
 @pytest.mark.filterwarnings("ignore::UserWarning")
 @pytest.mark.parametrize(
@@ -30,32 +26,6 @@ a[1] = 1 - a[0]  # ensures mixed bits when slicing
     ],
 )
 def test_randtests_can_default_kwargs(randtest, min_n):
-    randtest(a[:min_n])
-
-
-@pytest.mark.parametrize(
-    "randtest",
-    [
-        monobit,
-        blockfreq,
-        runs,
-        blockruns,
-        matrix,
-        spectral,
-        otm,
-        universal,
-        pytest.param(complexity, marks=pytest.mark.slow),
-        apen,
-        cumsum,
-    ],
-)
-def test_randtests_pass_random_bits(randtest):
-    result = randtest(a)
-    assert result.p > 0.01
-
-
-@pytest.mark.parametrize("randtest", [notm, serial, excursions, vexcursions])
-def test_mapped_randtests_pass_random_bits(randtest):
-    results = randtest(a)
-    for p in results.pvalues:
-        assert p > 0.01
+    a = urandom(min_n)
+    a[1] = 1 - a[0]  # ensure mixed bits
+    randtest(a)
